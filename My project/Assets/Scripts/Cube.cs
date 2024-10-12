481b548a77bd723ed8147480c4d6708f7e687b70
YarVolkotrub <at2tzik@gmail.com>
2024-10-12 17:00:35 +0300

    new

---
+++ My project/Assets/Scripts/Cube.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Renderer _renderer;
+
+    private void Awake()
+    {
+        gameObject.AddComponent(typeof(Rigidbody));
+        _rigidbody = GetComponent<Rigidbody>();
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void OnEnable()
+    {
+        _rigidbody.useGravity = true;
+        _renderer.GetComponent<Renderer>().material.color = Random.ColorHSV();
+    }
+}
---
+++ My project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private int _minCountCube = 2;
+    private int _maxCountCube = 6;
+    private float _chanceSeparation = 1;
+    private float _dividerChanceSeparation = 2;
+    private float _dividerScale = 2;
+    private float _explosionForce = 500;
+    private float _explosionRadius = 5;
+    private List<Rigidbody> _colliders = new List<Rigidbody>();
+
+    private void OnMouseUpAsButton()
+    {
+        if (CalculateChance())
+        {
+            CreateObjects();
+            Explode();
+        }
+        
+        Destroy(gameObject);
+        
+    }
+
+    private bool CalculateChance()
+    {
+        float chance = UnityEngine.Random.value;
+
+        if (chance <= _chanceSeparation)
+        {
+            _chanceSeparation /= _dividerChanceSeparation;
+            
+            return true;
+        }
+
+        return false;
+    }
+
+    private void CreateObjects()
+    {
+        int countCube = UnityEngine.Random.Range(_minCountCube, _maxCountCube + 1);
+        transform.localScale /= _dividerScale;
+
+        for (int i = 0; i < countCube; i++)
+        {
+            GameObject a = Instantiate(gameObject);
+            _colliders.Add(a.GetComponent<Collider>().attachedRigidbody);
+        }
+    }
+
+    private void Explode()
+    {
+        foreach (Rigidbody collider in _colliders)
+        {
+            collider.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+        }
+    }
+}
